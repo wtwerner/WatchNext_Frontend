d996e0906ba69e2d0c920366a02f8575231b14b8
wtwerner <wtwerner@gmail.com>
2020-12-04 10:47:33 -0600

    Refine search form

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ document.addEventListener("DOMContentLoaded", function(event){
     console.clear()
 
     const BACKEND_URL = 'http://localhost:3000';
-    const TMDB_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=462158256aa6d5d3eab60e67dcecfde2&query="'
+    const TMDB_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=462158256aa6d5d3eab60e67dcecfde2&query="';
 
-    const form = document.getElementById("form");
+    const form = document.getElementById('form');
+    const search = document.getElementById('movie-search');
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        main.innerHTML = '';
-        
-        const query = search.value;
+    form.addEventListener('click', function(event) {
+        event.preventDefault();
 
-        // if (query) {
-        //     showMovies(TMDB_URL + query);
+        let input = search.value;
+
+        console.log(input);
+        search.value = '';
+
+        // if (input) {
+        //     showMovies(TMDB_URL + input);
         //     search.value = "";
         // }
-    });
+    }, false);
 
     function fetchMovies() {
         fetch(`${BACKEND_URL}/movies`)
