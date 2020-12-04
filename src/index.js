091efb8ef223c06db1f3940dd6c16eda6c7c5a09
wtwerner <wtwerner@gmail.com>
2020-12-04 09:30:50 -0600

    Add search form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+document.addEventListener("DOMContentLoaded", function(event){
+    console.clear()
+
+    const BACKEND_URL = 'http://localhost:3000';
+    const TMDB_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=462158256aa6d5d3eab60e67dcecfde2&query="'
+
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        main.innerHTML = '';
+        
+        const query = search.value;
+
+        // if (query) {
+        //     showMovies(TMDB_URL + query);
+        //     search.value = "";
+        // }
+    });
+
+    function fetchMovies() {
+        fetch(`${BACKEND_URL}/movies`)
+        .then(response => response.json())
+        .then(parsedResponse => console.log(parsedResponse));
+    }
+
+    fetchMovies()
+});
