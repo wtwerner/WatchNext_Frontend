e41d82b459a87781980ec14173cf2e4ed2f84b59
wtwerner <wtwerner@gmail.com>
2020-12-05 11:37:21 -0600

    Adjust image size request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const BACKEND_URL = 'http://localhost:3000';
     const TMDB_URL = 'https://api.themoviedb.org/3/';
-    const IMG_URL = 'https://image.tmdb.org/t/p/w200';
+    const IMG_URL = 'https://image.tmdb.org/t/p/w92';
     const TMDB_APPEND = '?api_key=462158256aa6d5d3eab60e67dcecfde2'
 
 
