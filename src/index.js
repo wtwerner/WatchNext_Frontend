edfb09e49d14448c6f7d44dabcb406baaa8e69f7
wtwerner <wtwerner@gmail.com>
2020-12-06 20:19:45 -0600

    Separate watched view

--- src/index.js
+++ src/index.js
@@ -12,20 +12,31 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const watchListNavButton = document.getElementById('watchlist-nav');
     const searchNavButton = document.getElementById('search-nav');
+    const watchedNavButton = document.getElementById("watched-nav");
 
     const watchListView = document.getElementById("watchlist-view")
     const searchView = document.getElementById("search-view")
+    const watchedView = document.getElementById("watched-view")
 
     watchListNavButton.addEventListener('click', function(event) {
         event.preventDefault();
         searchView.hidden = true
         watchListView.hidden = false
+        watchedView.hidden = true
     })
 
     searchNavButton.addEventListener('click', function(event) {
         event.preventDefault();
         searchView.hidden = false
         watchListView.hidden = true
+        watchedView.hidden = true
+    })
+
+    watchedNavButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        searchView.hidden = false
+        watchListView.hidden = true
+        watchedView.hidden = false
     })
 
     searchButton.addEventListener('click', function(event) {
@@ -314,4 +325,5 @@ document.addEventListener("DOMContentLoaded", function(){
 
     fetchMovies()
     searchView.hidden = true
+    watchedView.hidden = true
 });
