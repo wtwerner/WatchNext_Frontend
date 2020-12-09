5ba3165c8aac8d8137cd4ca57c9b9d7ce9ad2e16
wtwerner <wtwerner@gmail.com>
2020-12-08 22:31:25 -0600

    Move views logic to own file

--- src/index.js
+++ src/index.js
@@ -10,37 +10,6 @@ document.addEventListener("DOMContentLoaded", function(){
     const searchButton = document.querySelector('#search');
     const search = document.getElementById('movie-search');
 
-    const watchListNavButton = document.getElementById('watchlist-nav');
-    const searchNavButton = document.getElementById('search-nav');
-    const watchedNavButton = document.getElementById("watched-nav");
-
-    const watchListView = document.getElementById("watchlist-view")
-    const searchView = document.getElementById("search-view")
-    const watchedView = document.getElementById("watched-view")
-
-    let movies = {}
-
-    watchListNavButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        searchView.hidden = true
-        watchListView.hidden = false
-        watchedView.hidden = true
-    })
-
-    searchNavButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        searchView.hidden = false
-        watchListView.hidden = true
-        watchedView.hidden = true
-    })
-
-    watchedNavButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        searchView.hidden = true
-        watchListView.hidden = true
-        watchedView.hidden = false
-    })
-
     searchButton.addEventListener('click', function(event) {
         event.preventDefault();
         const div = document.getElementById("search-results")
