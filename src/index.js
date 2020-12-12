81254f407f732fef3bdbb6f56317366c91b797c1
wtwerner <wtwerner@gmail.com>
2020-12-11 22:39:07 -0600

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -58,50 +58,6 @@ watchedNavButton.addEventListener('click', function(event) {
     watchedView.hidden = false
 })
 
-// function fetchMovies() {
-//     return fetch(BACKEND_URL+'/movies')
-//     .then(function(response) {
-//         return response.json()
-//     })
-//     .then(function(json) {
-//         let movies = json['data']
-//         movies.forEach(userMovieData => {
-//             fetchMovieData(userMovieData)
-//         })
-//     })
-// }
-
-// function fetchMovieData(movie) { 
-//     if(!(typeof movie === 'string' || movie instanceof String)) {
-//         if(movie.attributes.to_watch === true) {
-//             return fetch(TMDB_URL+'movie/'+movie.attributes.tmdb_id+TMDB_APPEND)
-//             .then(function(response) {
-//                 return response.json()
-//             })
-//             .then(function(json) {
-//                 createMovieCard(json)
-//             })
-//         } else {
-//             return fetch(TMDB_URL+'movie/'+movie.attributes.tmdb_id+TMDB_APPEND)
-//             .then(function(response) {
-//                 return response.json()
-//             })
-//             .then(function(json) {
-//                 createWatchedCard(json)
-//             })
-//         }
-//     } else {
-//         return fetch(TMDB_URL+'search/movie'+TMDB_APPEND+'&query='+movie)
-//         .then(function(response) {
-//             return response.json()
-//         })
-//         .then(function(json) {
-//             json.results.forEach(movie => {
-//                 createSearchCard(movie)
-//         })
-//     })
-// }}
-
 function addToWatchList(id) {
     fetch(BACKEND_URL+`/movies`, {
         method: 'POST',
