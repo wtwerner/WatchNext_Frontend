bb4cb5812a6904bd5f679375bcbcaa2ee9965924
wtwerner <wtwerner@gmail.com>
2020-12-18 16:40:39 -0600

    Fix modals on watched cards

--- src/cards.js
+++ src/cards.js
@@ -19,12 +19,13 @@ class Card {
         const modal = document.querySelector('.modal')
 
         const movie = this.movieData
+        let card = this
 
         img.src = IMG_URL + this.movieData.poster_path;
         imgDiv.className = "card-image";
         figure.className = "image is-2by3"
         figure.addEventListener('click', function() {
-            createModal(movie)
+            card.createModal(movie)
             modal.classList.add('is-active');
         })
         modalBackground.addEventListener('click', function() {
@@ -84,6 +85,21 @@ class Card {
 
     createSearchCard(column) {
         let main = document.getElementById("search-results");
+        let footer = document.querySelector('.card-footer')
+
+
+
+        let watchListButton = document.createElement("button")
+        watchListButton.id = "button-add"
+        watchListButton.innerHTML = "Add to watchlist"
+        watchListButton.className = "button card-footer-item"
+        watchListButton.setAttribute("movie_id", this.movieData.tmdb_id)
+        watchListButton.addEventListener('click', function(event) {
+            fetchMovieData(event.target.attributes.movie_id.value);
+            watchListButton.innerHTML = "Added to watch list"
+        })
+
+        footer.remove()
         main.appendChild(column)
     }
 
@@ -91,54 +107,55 @@ class Card {
         let card = document.getAttribute('data-tmdbid', this.id).parentElement
         card.remove()
     }
+
+    createModal(movie){
+        const modalDiv = document.getElementById('modal-content');
+        modalDiv.innerHTML = ""
+    
+        const box = document.createElement('div');
+        const media = document.createElement('article');
+        const image = document.createElement('div');
+        const figure = document.createElement('figure');
+        const imgSrc = document.createElement("img");
+        const mediaContent = document.createElement("div");
+        const content = document.createElement("div");
+        const title = document.createElement("h6");
+        const textBlock = document.createElement("div");
+        const text = document.createElement("p");
+        const genreBlock = document.createElement("div");
+        const genre = document.createElement("p");
+        const nav = document.createElement("nav");
+
+        box.className = "box"
+    
+        media.className = "media"
+        image.className = "media-left"
+        figure.className = "image is-96x96"
+        imgSrc.src = IMG_URL + movie.poster_path
+    
+        mediaContent.className = "media-content"
+        content.className = "content"
+        title.innerHTML = movie.title+" ("+movie.release_date.substring(0,4)+")";
+        textBlock.className = "block"
+        text.innerHTML = movie.overview
+        genreBlock.classname = "block"
+        genre.innerHTML = movie['genres'][0]['name']
+    
+        nav.className = "level"
+    
+        figure.appendChild(imgSrc)
+        image.appendChild(figure)
+        content.appendChild(title)
+        textBlock.appendChild(text)
+        genreBlock.appendChild(genre)
+        content.appendChild(textBlock)
+        content.appendChild(genreBlock)
+        mediaContent.appendChild(content)
+        media.appendChild(image)
+        media.appendChild(mediaContent)
+        media.appendChild(nav)
+        box.appendChild(media)
+        modalDiv.appendChild(box)
+    }
 }
 
-function createModal(movie){
-    const modalDiv = document.getElementById('modal-content');
-    modalDiv.innerHTML = ""
-
-    const box = document.createElement('div');
-    const media = document.createElement('article');
-    const image = document.createElement('div');
-    const figure = document.createElement('figure');
-    const imgSrc = document.createElement("img");
-    const mediaContent = document.createElement("div");
-    const content = document.createElement("div");
-    const title = document.createElement("h6");
-    const textBlock = document.createElement("div");
-    const text = document.createElement("p");
-    const genreBlock = document.createElement("div");
-    const genre = document.createElement("p");
-    const nav = document.createElement("nav");
-
-    box.className = "box"
-
-    media.className = "media"
-    image.className = "media-left"
-    figure.className = "image is-96x96"
-    imgSrc.src = IMG_URL + movie.poster_path
-
-    mediaContent.className = "media-content"
-    content.className = "content"
-    title.innerHTML = movie.title+" ("+movie.release_date.substring(0,4)+")";
-    textBlock.className = "block"
-    text.innerHTML = movie.overview
-    genreBlock.classname = "block"
-    genre.innerHTML = movie['genres'][0]['name']
-
-    nav.className = "level"
-
-    figure.appendChild(imgSrc)
-    image.appendChild(figure)
-    content.appendChild(title)
-    textBlock.appendChild(text)
-    genreBlock.appendChild(genre)
-    content.appendChild(textBlock)
-    content.appendChild(genreBlock)
-    mediaContent.appendChild(content)
-    media.appendChild(image)
-    media.appendChild(mediaContent)
-    media.appendChild(nav)
-    box.appendChild(media)
-    modalDiv.appendChild(box)
-}
